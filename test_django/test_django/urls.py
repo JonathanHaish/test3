d6a6cde8a1ad1e37b5a843bdca8d0a7cc598a17b
Your Name <you@example.com>
2024-07-14 13:39:23 +0300

    new version

--- test_django/test_django/urls.py
+++ test_django/test_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from test_django.views import *
+from test_django import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
