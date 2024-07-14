445696b518e9c0f84d4802363a7144604d3a9b6e
Your Name <you@example.com>
2024-07-14 12:51:34 +0300

    new version

--- test_django/test_django/urls.py
+++ test_django/test_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from test_django.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', views.home, name='home'),
+     
 ]
---
+++ test_django/test_django/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+
+def home(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not null:
+                login(request, user)
+                return redirect('dashboard')  # Redirect to a dashboard page after login
+            else:
+                return render(request, 'home.html', {'form': form, 'error': 'Invalid credentials'})
+    else:
+        
+    return render(request, 'home.html', {'form': 'ddd'})
